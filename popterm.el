2c00a214e25d38f28e9954e18cddd12db12f7bff
hadoopchetan@gmail.com <hadoopchetan@gmail.com>
2026-03-10 21:56:47 -0400

    feat(authorship): update author and license information
    - Updated author name and email to Chetan Koneru - Added full GNU
    GPL v3 license text with proper formatting and attribution - Updated
    repository URL to match new GitHub organization - Improved license
    section formatting for clarity and compliance

--- popterm.el
+++ popterm.el
@@ -2,18 +2,28 @@
 
 ;; Copyright (C) 2026
 
-;; Author: ckaiatwork
+;; Author: Chetan Koneru <kchetan.hadoop@gmail.com>
 ;; Version: 0.9.1
 ;; Package-Requires: ((emacs "28.1") (posframe "1.4.0"))
 ;; Keywords: terminals, convenience, tools
-;; URL: https://github.com/user/popterm.el
+;; URL: https://github.com/CsBigDataHub/popterm.el
 
 ;; This file is not part of GNU Emacs.
 ;;
-;; This program is free software; you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation; either version 3, or
-;; (at your option) any later version.
+;;; License:
+;;
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 ;;
